1cdd153445105fca0f76d8807dda34a4f189a2f8
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2022-01-17 17:22:29 +0100

    secondo commit

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ const primoStudente = {
 
 for(let key in primoStudente) {
     console.log(primoStudente[key]);
-}
+};
 
+
+const secondoStudente = {
+    nome: 'Marco',
+    cognome :'Rossi',
+    età : 5
+};
+
+const terzoStudente = {
+    nome: 'Carlo',
+    cognome:'verdi',
+    età:70 
+};
+
+let arrayStudenti = [primoStudente , secondoStudente , terzoStudente];
+
+for (let i = 0; i < arrayStudenti.length ;i++){
+
+    for(let key in arrayStudenti[i]){
+        console.log(arrayStudenti[i][key])
+    }
+
+    //console.log(arrayStudenti[i].nome + " " + arrayStudenti[i].cognome  + " " + arrayStudenti[i].età )
+  
+};
 //console.log(primoStudente.nome);
