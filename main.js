94f1e4cb1c3420b7d313ba2d932a5076daa0ee5f
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2022-01-17 16:59:55 +0100

    primo commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const primoStudente = {
+    nome : 'Luca',
+    cognome : 'Bianchi',
+    età : 30
+};
+
+for(let key in primoStudente) {
+    console.log(primoStudente[key]);
+}
+
+//console.log(primoStudente.nome);
